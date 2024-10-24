a8ccfa6cbad38bf1902746f9588ccf2fdf2803b2
Drakww <jorgevilcataipe@gmail.com>
2024-10-24 12:26:53 -0500

    Funcion pago implementada correctamente

--- src/main/java/avance/integrador/controlador/AdministradorControlador.java
+++ src/main/java/avance/integrador/controlador/AdministradorControlador.java
@@ -4,15 +4,26 @@
  */
 package avance.integrador.controlador;
 
+import avance.integrador.modelo.PagoMatricula;
+import avance.integrador.servicio.IPagoMatriculaServicio;
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 
 @Controller
 public class AdministradorControlador {
 
+    @Autowired
+    private IPagoMatriculaServicio pagoServicio;
+    
     @GetMapping("/administrador")
-    public String showDashboard() {
-        return "administrador"; // Nombre de la plantilla HTML para el dashboard
+    public String Pagos(Model model) {
+        List<PagoMatricula> pagos = pagoServicio.listarPagosMatricula();
+        model.addAttribute("pagos",pagos);
+        
+        return "administrador"; // Nombre de la plantilla HTML para el admistrador
     }
 }
---
+++ src/main/java/avance/integrador/repositorio/PagoMatriculaRepositorio.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package avance.integrador.repositorio;
+
+import avance.integrador.modelo.PagoMatricula;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+/**
+ *
+ * @author PC
+ */
+public interface PagoMatriculaRepositorio extends JpaRepository<PagoMatricula, Integer>{    
+}
