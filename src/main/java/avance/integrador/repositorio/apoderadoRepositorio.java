676187e3fabbcc8e2078aed5daae48099db4a488
joshep8 <playjos10@gmail.com>
2024-11-12 17:19:35 -0500

    Reemplazo del proyecto completo

--- src/main/java/avance/integrador/repositorio/apoderadoRepositorio.java
+++
@@ -1,8 +0,0 @@
-package avance.integrador.repositorio;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import avance.integrador.modelo.apoderado;
-
-public interface apoderadoRepositorio extends JpaRepository<apoderado, Integer> {
-    // Puedes añadir métodos personalizados si es necesario
-}
--- src/main/java/avance/integrador/repositorio/solicitudRepositorio.java
+++
@@ -1,9 +0,0 @@
-package avance.integrador.repositorio;
-
-import avance.integrador.modelo.Solicitud;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-
-public interface solicitudRepositorio extends JpaRepository<Solicitud, Integer> {
-    // Puedes añadir métodos personalizados si es necesario
-}
